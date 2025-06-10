f71826ba7d35a3f6ba5531544f45043b26289679
mihaldenn <mhd2.ionita@gmail.com>
2025-06-10 14:21:46 +0200

    Update app.py

--- ferrari_app/app.py
+++ ferrari_app/app.py
@@ -46,8 +46,8 @@ stile_ferrari()
 
 # ─────────────────────────────────────────────
 # SEZIONE LOGO CENTRATO E TITOLO
-col1, col2, col3 = st.columns([2, 1, 1])
-with col1:
+
+with:
     logo = Image.open("./ferrari_app/logo_ferrari.jpg")
     st.image(logo, width=150)
 
