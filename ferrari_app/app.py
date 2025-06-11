33afcf550b101e4c40d160c171b6e9cbba070355
mihaldenn <mhd2.ionita@gmail.com>
2025-06-11 16:12:25 +0200

    Update app.py

--- ferrari_app/app.py
+++ ferrari_app/app.py
@@ -56,9 +56,15 @@ st.header("Configura Prodotti e Costi")
 
 st.markdown("""
     <style>
-        input[type="checkbox"]:checked {
-            background-color: yellow !important;
-            border: 2px solid black !important;
+        /* Forza le checkbox a occupare tutto lo spazio disponibile */
+        .stDataFrame input[type="checkbox"], .stDataEditor input[type="checkbox"] {
+            width: 100% !important;
+            height: 100% !important;
+            display: flex !important;
+            justify-content: center !important;
+            align-items: center !important;
+            background-color: white !important;
+            box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.1) !important;
         }
     </style>
 """, unsafe_allow_html=True)
