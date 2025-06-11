d7adcdcfdb63ade605fd5522479b57e4be7574b3
mihaldenn <mhd2.ionita@gmail.com>
2025-06-11 15:42:53 +0200

    Update app.py

--- ferrari_app/app.py
+++ ferrari_app/app.py
@@ -67,8 +67,7 @@ data_iniziale = pd.DataFrame({
         "MODULARI", "VETRO", "BAGNI", "ELETTRICO", "ARIA", "VMC", "ARREDI", "SOPPALCO"
     ],
     "Costo/mq": [60, 110, 80, 150, 190, 230, 100, 190, 250, 250, 150, 600],
-    "PT": [False] * 11 + [None],  # 🔹 PT sarà nascosto solo per il Soppalco
-    "P1": [False] * 11 + [None],  # 🔹 P1 sarà nascosto solo per il Soppalco
+    "PT/P1": [False] * 11 + [False],  # 🔹 Per "SOPPALCO", unisci PT e P1
     "Soppalco": [None] * 11 + [False],  # 🔹 Solo il Soppalco avrà una casella di spunta unica
     "Stima PT": [0.0] * len(prodotti),
     "Stima P1": [0.0] * len(prodotti),
