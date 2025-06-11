30d671f5091cc746eda829f30c70fc1bd21c101b
mihaldenn <mhd2.ionita@gmail.com>
2025-06-11 15:55:54 +0200

    Update app.py

--- ferrari_app/app.py
+++ ferrari_app/app.py
@@ -23,7 +23,7 @@ st.markdown("""
         input[type="checkbox"] {
             width: 100%;
             height: 100%;
-            margin: auto; /* 🔹 Centra le checkbox nella cella */
+            margin: auto; /*
             display: flex;
             justify-content: center;
             align-items: center;
