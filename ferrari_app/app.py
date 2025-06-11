445542cba9577e07c40d3ac94494bd656fdb4a19
mihaldenn <mhd2.ionita@gmail.com>
2025-06-11 16:38:51 +0200

    Update app.py

--- ferrari_app/app.py
+++ ferrari_app/app.py
@@ -82,11 +82,20 @@ data_iniziale = pd.DataFrame({
     ],
     "Costo/mq": [60, 110, 80, 150, 190, 230, 100, 190, 250, 250, 150, 600],
     "PT": [False] * 11 + [False],  # Mantiene separato per tutti tranne il Soppalco
-    "P1": [False] * 11 + [""],  # 🔹 Usa "None" per nascondere la colonna P1 solo per il Soppalco
+    "P1": [False] * 11 + [False],  # 🔹 Usa "None" per nascondere la colonna P1 solo per il Soppalco
     "Stima PT": [0.0] * len(prodotti),
     "Stima P1": [0.0] * len(prodotti),
     "Stima Totale": [0.0] * len(prodotti)
 })
+
+st.markdown("""
+    <style>
+        /* Nasconde la seconda checkbox di SOPPALCO */
+        tr:nth-child(12) td:nth-child(4) {
+            visibility: hidden !important;
+        }
+    </style>
+""", unsafe_allow_html=True)
 
 # 🔹 Inizializza la sessione con dati validi
 if "editor" not in st.session_state or not isinstance(st.session_state["editor"], list) or not st.session_state["editor"]:
